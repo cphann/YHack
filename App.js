8f4e3f4249f2fdffae832b482099a2efb62666dc
nagena <nlatifi@wesleyan.edu>
2024-03-30 20:24:36 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import Homepage from './Homepage';
@@ -11,22 +11,26 @@ import Settings from './Settings';
 const Stack = createNativeStackNavigator();
 
 // Define a custom tab bar component
-const CustomTabBar = ({ navigation }) => (
-  <View style={styles.tabBar}>
-    <TouchableOpacity onPress={() => navigation.navigate('Homepage')} style={styles.tabItem}>
-      <Text>Home</Text>
-    </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigation.navigate('Community')} style={styles.tabItem}>
-      <Text>Community</Text>
-    </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigation.navigate('CurrentSong')} style={styles.tabItem}>
-      <Text>Current Song</Text>
-    </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigation.navigate('Settings')} style={styles.tabItem}>
-      <Text>Settings</Text>
-    </TouchableOpacity>
-  </View>
-);
+const CustomTabBar = () => {
+  const navigation = useNavigation(); // Use useNavigation hook to access navigation object
+
+  return (
+    <View style={styles.tabBar}>
+      <TouchableOpacity onPress={() => navigation.navigate('Homepage')} style={styles.tabItem}>
+        <Text>Home</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('Community')} style={styles.tabItem}>
+        <Text>Community</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('CurrentSong')} style={styles.tabItem}>
+        <Text>Current Song</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('Settings')} style={styles.tabItem}>
+        <Text>Settings</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const App = () => {
   return (
